2a8510f5d9a01de9ca781edfb8896bf6d66f2d30
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2023-01-10 16:24:16 +0900

    [INIT]: 초기 프로젝트 설정.

---
+++ src/main/java/com/prgrms/be/intermark/IntermarkApplication.java
@@ -0,0 +1,13 @@
+package com.prgrms.be.intermark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntermarkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntermarkApplication.class, args);
+    }
+
+}
