5d10fcea1665b542d90089d79e5ab480103873d6
권성준 <tjdwns1865@naver.com>
2023-01-13 20:59:45 +0900

    [FEAT] 티켓 엔티티 설계, 연관관계 설정

--- src/main/java/com/prgrms/be/intermark/domain/schedule_seat/ScheduleSeat.java
+++ src/main/java/com/prgrms/be/intermark/domain/schedule_seat/ScheduleSeat.java
@@ -1,14 +1,27 @@
 package com.prgrms.be.intermark.domain.schedule_seat;
 
-import com.prgrms.be.intermark.domain.performance_stadium.PerformanceStadium;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
 import com.prgrms.be.intermark.domain.schedule.Schedule;
 import com.prgrms.be.intermark.domain.seat.Seat;
+import com.prgrms.be.intermark.domain.ticket.Ticket;
+
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
-import java.util.Objects;
 
 @Entity
 @Table(name = "seat")
@@ -27,6 +40,16 @@ public class ScheduleSeat {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "seat_id")
     private Seat seat;
+
+    @OneToMany(mappedBy = "scheduleSeat")
+    private List<Ticket> tickets = new ArrayList<>();
+
+    @Builder
+    public ScheduleSeat(Schedule schedule, Seat seat, List<Ticket> tickets) {
+        this.schedule = schedule;
+        this.seat = seat;
+        this.tickets = tickets;
+    }
 
     public void setSchedule(Schedule schedule) {
         if (Objects.nonNull(this.schedule)) {
--- src/main/java/com/prgrms/be/intermark/domain/user/User.java
+++ src/main/java/com/prgrms/be/intermark/domain/user/User.java
@@ -1,16 +1,33 @@
 package com.prgrms.be.intermark.domain.user;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+import org.springframework.lang.Nullable;
+
+import com.prgrms.be.intermark.domain.ticket.Ticket;
+
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.Length;
-import org.springframework.lang.Nullable;
-
-import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import java.util.Date;
 
 @Entity
 @Table(name = "user",
@@ -55,8 +72,11 @@ public class User {
     @Column(name = "birth")
     private Date birth;
 
+    @OneToMany(mappedBy = "user")
+    private List<Ticket> tickets = new ArrayList<>();
+
     @Builder
-    public User(Social social, String socialId, String refreshToken, String username, UserRole role, boolean isDeleted, Date birth) {
+    public User(Social social, String socialId, String refreshToken, String username, UserRole role, boolean isDeleted, Date birth, List<Ticket> tickets) {
         this.social = social;
         this.socialId = socialId;
         this.refreshToken = refreshToken;
@@ -64,6 +84,7 @@ public class User {
         this.role = role;
         this.isDeleted = isDeleted;
         this.birth = birth;
+        this.tickets = tickets;
     }
 
     public void setBirth(Date birth) {
