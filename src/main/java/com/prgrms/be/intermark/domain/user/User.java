b51006c0e1071d466051ceb65504dd25d71f05ae
thguss <thgus345@naver.com>
2023-01-13 16:09:33 +0900

    [ADD] getter 추가

--- src/main/java/com/prgrms/be/intermark/domain/user/User.java
+++ src/main/java/com/prgrms/be/intermark/domain/user/User.java
@@ -2,6 +2,7 @@ package com.prgrms.be.intermark.domain.user;
 
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.lang.Nullable;
@@ -15,6 +16,7 @@ import java.util.Date;
 @Table(name = "user",
         uniqueConstraints = {@UniqueConstraint(name = "social_uk", columnNames = {"social", "social_id"})})
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class User {
 
     @Id
