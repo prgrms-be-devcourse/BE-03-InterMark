c139658dc5234f5a4831b7dfe0750df90b4e89e2
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2023-01-17 04:31:53 +0900

    [ADD] : jwt에서 이용되는 상수들을 추가.
    - jwt에서 사용하는 value 값을 찾기 위한 key들을 추가.
    CLose feature/이수영_#27

--- src/main/java/com/prgrms/be/intermark/auth/constant/JwtConstants.java
+++ src/main/java/com/prgrms/be/intermark/auth/constant/JwtConstants.java
@@ -5,7 +5,8 @@ public final class JwtConstants {
     public static final long REFRESH_TOKEN_EXP = 604800000;
     public static final int REFRESH_TOKEN_COOKIE_MAX_AGE = (int) REFRESH_TOKEN_EXP / 60;
     public static final String REFRESH_TOKEN_COOKIE_NAME = "refresh_token";
-
+    public static final String HEADER_AUTHORIZATION = "Authorization";
+    public static final String TOKEN_PREFIX = "Bearer ";
     public static final String OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME = "oauth2_auth_request";
-
+    public static final String AUTHORITIES_KEY = "role";
 }
