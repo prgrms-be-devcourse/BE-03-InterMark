f7075c27e8bab9d35dee5f019e0f7163808d023a
권성준 <tjdwns1865@naver.com>
2023-01-16 18:43:39 +0900

    [FEAT] : 좌석 조회 API 구현

--- src/main/java/com/prgrms/be/intermark/domain/schedule_seat/repository/ScheduleSeatRepository.java
+++ src/main/java/com/prgrms/be/intermark/domain/schedule_seat/repository/ScheduleSeatRepository.java
@@ -1,13 +1,20 @@
 package com.prgrms.be.intermark.domain.schedule_seat.repository;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import com.prgrms.be.intermark.domain.schedule.Schedule;
 import com.prgrms.be.intermark.domain.schedule_seat.ScheduleSeat;
 import com.prgrms.be.intermark.domain.seat.Seat;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
 
 public interface ScheduleSeatRepository extends JpaRepository<ScheduleSeat, Long> {
 
     Optional<ScheduleSeat> findByScheduleAndSeat(Schedule schedule, Seat seat);
+
+    @Query("select s from ScheduleSeat s where s.schedule.id = :scheduleId ")
+    List<ScheduleSeat> findScheduleSeatsByScheduleId(@Param("scheduleId") Long scheduleId);
 }
