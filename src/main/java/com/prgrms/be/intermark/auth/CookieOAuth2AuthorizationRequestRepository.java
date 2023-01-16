792b7b26ddada250f057ecd3a5d74a141ea801fa
박세준 <tpwns6693@gmail.com>
2023-01-17 01:50:19 +0900

    [CHORE] : CookieOAuth2AuthorizationRequestRepository에 CookieUtils 사용 및 Constant 분리 - CookieUtils를 사용하도록 코드 변경 - Constant를 분리

--- src/main/java/com/prgrms/be/intermark/auth/CookieOAuth2AuthorizationRequestRepository.java
+++ src/main/java/com/prgrms/be/intermark/auth/CookieOAuth2AuthorizationRequestRepository.java
@@ -1,24 +1,20 @@
 package com.prgrms.be.intermark.auth;
 
+import com.prgrms.be.intermark.util.CookieUils;
 import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
 import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
 import org.springframework.util.SerializationUtils;
-import org.springframework.web.util.WebUtils;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import java.util.Base64;
 import java.util.Optional;
 
-import static org.springframework.web.util.WebUtils.getCookie;
+import static com.prgrms.be.intermark.auth.constant.JwtConstants.OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME;
 
 public class CookieOAuth2AuthorizationRequestRepository implements AuthorizationRequestRepository<OAuth2AuthorizationRequest> {
 
-    public static final String REDIRECT_URI_PARAMCOOKIE_NAME = "redirect_uri";
-    public static final String REFRESH_TOKEN = "refresh_token";
-    private static final String OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME = "oauth2_auth_request";
 
     private final String cookieName;
 
@@ -46,11 +42,8 @@ public class CookieOAuth2AuthorizationRequestRepository implements Authorization
             getCookie(request).ifPresent(cookie -> clear(cookie, response));
         } else {
             String value = Base64.getUrlEncoder().encodeToString(SerializationUtils.serialize(authorizationRequest));
-            Cookie cookie = new Cookie(cookieName, value);
-            cookie.setPath("/");
-            cookie.setHttpOnly(true);
-            cookie.setMaxAge(cookieExpireSeconds);
-            response.addCookie(cookie);
+
+            CookieUils.addCookie(cookieName, value, cookieExpireSeconds, response);
         }
     }
 
@@ -61,17 +54,18 @@ public class CookieOAuth2AuthorizationRequestRepository implements Authorization
 
     @Override
     public OAuth2AuthorizationRequest removeAuthorizationRequest(HttpServletRequest request, HttpServletResponse response) {
-        return getCookie(request)
-                .map(cookie -> {
-                    OAuth2AuthorizationRequest oauth2Request = getOAuth2AuthorizationRequest(cookie);
-                    clear(cookie, response);
-                    return oauth2Request;
-                })
-                .orElse(null);
+        return
+                getCookie(request)
+                        .map(cookie -> {
+                            OAuth2AuthorizationRequest oauth2Request = getOAuth2AuthorizationRequest(cookie);
+                            clear(cookie, response);
+                            return oauth2Request;
+                        })
+                        .orElse(null);
     }
 
     private Optional<Cookie> getCookie(HttpServletRequest request) {
-        return Optional.ofNullable(WebUtils.getCookie(request, cookieName));
+        return CookieUils.getCookieByName(cookieName, request);
     }
 
     private void clear(Cookie cookie, HttpServletResponse response) {
--- src/main/java/com/prgrms/be/intermark/auth/constant/JwtConstants.java
+++ src/main/java/com/prgrms/be/intermark/auth/constant/JwtConstants.java
@@ -5,4 +5,7 @@ public final class JwtConstants {
     public static final long REFRESH_TOKEN_EXP = 604800000;
     public static final int REFRESH_TOKEN_COOKIE_MAX_AGE = (int) REFRESH_TOKEN_EXP / 60;
     public static final String REFRESH_TOKEN_COOKIE_NAME = "refresh_token";
+
+    public static final String OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME = "oauth2_auth_request";
+
 }
