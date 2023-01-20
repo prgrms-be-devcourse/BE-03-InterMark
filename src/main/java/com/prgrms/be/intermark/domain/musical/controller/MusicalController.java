8799a7b11b3d2cab58dc516d5014457c3f167f6f
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2023-01-20 23:58:45 +0900

    [FIX] : 뮤지컬 목록 조회 API 수정 (#75)
    ## PR Description 🗒️
    closed #74 
    - 공연 목록 조회 API 리팩토링
    
    ## 추가 사항 및 설명 ➕
    
    ## 수정 사항 및 이유 🔄
    - DB 리펙토링으로 인해 공연 목록 조회 API 코드가 수정되었습니다.
    
    ## To Reviewers 🙏

---
+++ src/main/java/com/prgrms/be/intermark/domain/musical/controller/MusicalController.java
@@ -0,0 +1,27 @@
+package com.prgrms.be.intermark.domain.musical.controller;
+
+import com.prgrms.be.intermark.common.dto.page.dto.PageResponseDTO;
+import com.prgrms.be.intermark.domain.musical.dto.MusicalResponseDTO;
+import com.prgrms.be.intermark.domain.musical.model.Musical;
+import com.prgrms.be.intermark.domain.musical.service.MusicalService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/musicals")
+@RequiredArgsConstructor
+public class MusicalController {
+
+    private final MusicalService musicalService;
+
+    @GetMapping
+    public ResponseEntity<PageResponseDTO<Musical, MusicalResponseDTO>> getAllMusicals(Pageable pageable) {
+        PageResponseDTO<Musical, MusicalResponseDTO> musicals = musicalService.findAllMusicals(pageable);
+
+        return ResponseEntity.ok(musicals);
+    }
+}
