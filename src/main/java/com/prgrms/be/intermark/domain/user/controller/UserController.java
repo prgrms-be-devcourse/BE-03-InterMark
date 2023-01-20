702e5e4f6cf87fb4306df521f6a2842f3ef4aeb4
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2023-01-20 21:35:24 +0900

    [FEAT] : UserController 추가.
    - 유저 id를 이용하여 유저의 정보를 조회하기 위한 findUser 메소드 추가.
    
    Close feature/이수영_#12

---
+++ src/main/java/com/prgrms/be/intermark/domain/user/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.prgrms.be.intermark.domain.user.controller;
+
+import com.prgrms.be.intermark.domain.user.dto.UserInfoResponseDTO;
+import com.prgrms.be.intermark.domain.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    // TODO : 발생할 수 있는 EntityNotFoundException에 대한 핸들러 없는 경우 추가.
+    private final UserService userService;
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserInfoResponseDTO> findUser (@PathVariable Long userId) {
+        UserInfoResponseDTO userInfoResponseDTO = userService.findById(userId);
+        return ResponseEntity.ok(userInfoResponseDTO);
+    }
+}
