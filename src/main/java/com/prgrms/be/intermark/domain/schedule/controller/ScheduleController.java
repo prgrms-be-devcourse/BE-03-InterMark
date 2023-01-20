98346a6135ae3a623492140af042664b85412dfb
thguss <thgus345@naver.com>
2023-01-20 23:35:27 +0900

    [DEL] 제네릭 Raw Type 지양

--- src/main/java/com/prgrms/be/intermark/domain/schedule/controller/ScheduleController.java
+++ src/main/java/com/prgrms/be/intermark/domain/schedule/controller/ScheduleController.java
@@ -20,7 +20,7 @@ public class ScheduleController {
     private final ScheduleService scheduleService;
 
     @PostMapping
-    public ResponseEntity createSchedule(@RequestBody @Valid ScheduleCreateRequestDTO requestDto) {
+    public ResponseEntity<String> createSchedule(@RequestBody @Valid ScheduleCreateRequestDTO requestDto) {
         Long scheduleId = scheduleService.createSchedule(requestDto);
 
         return ResponseEntity
