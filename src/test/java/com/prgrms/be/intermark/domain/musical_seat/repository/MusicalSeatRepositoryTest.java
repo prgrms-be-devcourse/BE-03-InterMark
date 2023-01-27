e65a0b4561196b321378da6b798a749a3ac150db
권성준 <tjdwns1865@naver.com>
2023-01-28 04:30:54 +0900

    [TEST] : MusicalSeatRepository save 메서드 테스트 작성
    - MusicalSeat 엔티티의 검증 조건에 맞게 저장에 성공, 실패하는지 여부

---
+++ src/test/java/com/prgrms/be/intermark/domain/musical_seat/repository/MusicalSeatRepositoryTest.java
@@ -0,0 +1,118 @@
+package com.prgrms.be.intermark.domain.musical_seat.repository;
+
+import com.prgrms.be.intermark.domain.musical.model.Musical;
+import com.prgrms.be.intermark.domain.musical.repository.MusicalRepository;
+import com.prgrms.be.intermark.domain.musical_seat.model.MusicalSeat;
+import com.prgrms.be.intermark.domain.seat.model.Seat;
+import com.prgrms.be.intermark.domain.seat.repository.SeatRepository;
+import com.prgrms.be.intermark.domain.seatgrade.model.SeatGrade;
+import com.prgrms.be.intermark.domain.seatgrade.repository.SeatGradeRepository;
+import com.prgrms.be.intermark.domain.stadium.model.Stadium;
+import com.prgrms.be.intermark.domain.stadium.repository.StadiumRepository;
+import com.prgrms.be.intermark.domain.user.User;
+import com.prgrms.be.intermark.domain.user.repository.UserRepository;
+import com.prgrms.be.intermark.domain.util.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import javax.validation.ConstraintViolationException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DataJpaTest
+class MusicalSeatRepositoryTest {
+
+    @Autowired
+    private MusicalSeatRepository musicalSeatRepository;
+
+    @Autowired
+    private StadiumRepository stadiumRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private MusicalRepository musicalRepository;
+
+    @Autowired
+    private SeatRepository seatRepository;
+
+    @Autowired
+    private SeatGradeRepository seatGradeRepository;
+
+    private final String thumbnailUrl = "https://intermark.com";
+    private final Stadium stadium = StadiumProvider.createStadium();
+    private final User user = UserProvider.createUser();
+    private final Musical musical = MusicalProvider.createMusical(thumbnailUrl, stadium, user);
+    private final Seat seat = SeatProvider.createSeat(stadium);
+    private final SeatGrade seatGrade = SeatGradeProvider.createSeatGrade(musical);
+    private final MusicalSeat musicalSeat = MusicalSeatProvider.createMusicalSeat(musical, seat, seatGrade);
+
+    @BeforeEach
+    void setUp() {
+        stadiumRepository.save(stadium);
+        userRepository.save(user);
+        musicalRepository.save(musical);
+        seatRepository.save(seat);
+        seatGradeRepository.save(seatGrade);
+    }
+
+    @Nested
+    @DisplayName("save")
+    class Save {
+
+        @Test
+        @DisplayName("성공 - 정상적인 뮤지컬좌석 값이 입력되면 저장에 성공한다")
+        void saveSuccess() {
+            // given & when
+            MusicalSeat savedMusicalSeat = musicalSeatRepository.save(musicalSeat);
+
+            // then
+            assertThat(savedMusicalSeat.getId()).isNotNull();
+            assertThat(savedMusicalSeat)
+                    .usingRecursiveComparison()
+                    .ignoringFields("id")
+                    .isEqualTo(musicalSeat);
+        }
+
+
+        @Test
+        @DisplayName("실패 - 연관된 뮤지컬 값이 없으면 저장에 실패한다")
+        void saveFailByNoMusical() {
+            // given
+            MusicalSeat musicalSeat = MusicalSeatProvider.createMusicalSeat(null, seat, seatGrade);
+
+            // when & then
+            assertThatThrownBy(() -> musicalSeatRepository.save(musicalSeat))
+                    .isInstanceOf(ConstraintViolationException.class);
+        }
+
+        @Test
+        @DisplayName("실패 - 연관된 좌석 값이 없으면 저장에 실패한다")
+        void saveFailByNoSeat() {
+            // given
+            MusicalSeat musicalSeat = MusicalSeatProvider.createMusicalSeat(musical, null, seatGrade);
+
+            // when & then
+            assertThatThrownBy(() -> musicalSeatRepository.save(musicalSeat))
+                    .isInstanceOf(ConstraintViolationException.class);
+        }
+
+        @Test
+        @DisplayName("실패 - 연관된 좌석 등급 값이 없으면 저장에 실패한다")
+        void saveFailByNoSeatGrade() {
+            // given
+            MusicalSeat musicalSeat = MusicalSeatProvider.createMusicalSeat(musical, seat, null);
+
+            // when & then
+            assertThatThrownBy(() -> musicalSeatRepository.save(musicalSeat))
+                    .isInstanceOf(ConstraintViolationException.class);
+        }
+    }
+
+}
