85ade351d3a863609b9dd6928a58eee9f5da98cf
권성준 <tjdwns1865@naver.com>
2023-01-28 03:36:25 +0900

    [TEST] : MusicalDetailImage 필드를 검증하는 책임을 MusicalDetailImageServiceTest 에서 분리

--- src/test/java/com/prgrms/be/intermark/domain/musical/service/MusicalDetailImageServiceTest.java
+++ src/test/java/com/prgrms/be/intermark/domain/musical/service/MusicalDetailImageServiceTest.java
@@ -1,22 +1,21 @@
 package com.prgrms.be.intermark.domain.musical.service;
 
-import com.prgrms.be.intermark.domain.musical.model.Genre;
 import com.prgrms.be.intermark.domain.musical.model.Musical;
 import com.prgrms.be.intermark.domain.musical.model.MusicalDetailImage;
-import com.prgrms.be.intermark.domain.musical.model.ViewRating;
 import com.prgrms.be.intermark.domain.musical.repository.MusicalDetailImageRepository;
 import com.prgrms.be.intermark.domain.stadium.model.Stadium;
-import com.prgrms.be.intermark.domain.user.Social;
 import com.prgrms.be.intermark.domain.user.User;
-import com.prgrms.be.intermark.domain.user.UserRole;
+import com.prgrms.be.intermark.domain.util.MusicalProvider;
+import com.prgrms.be.intermark.domain.util.StadiumProvider;
+import com.prgrms.be.intermark.domain.util.UserProvider;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.time.LocalDate;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -32,56 +31,38 @@ class MusicalDetailImageServiceTest {
     private MusicalDetailImageRepository musicalDetailImageRepository;
 
 
-    String thumbnailUrl = "abcedf";
-    Stadium stadium = Stadium.builder()
-            .name("예술의 전당")
-            .address("서울특별시")
-            .imageUrl("abcefg")
-            .build();
-    User user = User.builder()
-            .birth(LocalDate.of(1997, 10, 10))
-            .social(Social.GOOGLE)
-            .socialId("intermark")
-            .refreshToken("abcdefg")
-            .nickname("인터마크 관리자")
-            .role(UserRole.ADMIN)
-            .isDeleted(false)
-            .email("intermark@gmail.com")
-            .build();
-    Musical musical = Musical.builder()
-            .title("제목")
-            .viewRating(ViewRating.ADULT)
-            .genre(Genre.DRAMA)
-            .description("설명")
-            .startDate(LocalDate.of(2023, 1, 1))
-            .endDate(LocalDate.of(2023, 2, 1))
-            .runningTime(60)
-            .thumbnailUrl(thumbnailUrl)
-            .stadium(stadium)
-            .user(user)
-            .build();
+    @Nested
+    @DisplayName("save")
+    class Save {
 
-    @Test
-    @DisplayName("성공 - 정상 뮤지컬 상세 이미지 값이 들어오면 저장에 성공한다 - save")
-    void save() {
-        // given
-        MusicalDetailImage musicalDetailImage1 = MusicalDetailImage.builder()
-                .originalFileName("업로드 파일1")
-                .imageUrl("a")
-                .musical(musical)
-                .build();
-        MusicalDetailImage musicalDetailImage2 = MusicalDetailImage.builder()
-                .originalFileName("업로드 파일2")
-                .imageUrl("ab")
-                .musical(musical)
-                .build();
-        List<MusicalDetailImage> musicalDetailImages = List.of(musicalDetailImage1, musicalDetailImage2);
-        when(musicalDetailImageRepository.save(any(MusicalDetailImage.class))).thenReturn(any(MusicalDetailImage.class));
+        private final String thumbnailUrl = "https://intermark.com";
+        private final Stadium stadium = StadiumProvider.createStadium();
+        private final User user = UserProvider.createUser();
+        private final Musical musical = MusicalProvider.createMusical(thumbnailUrl, stadium, user);
 
-        // when
-        musicalDetailImageService.save(musicalDetailImages);
+        @Test
+        @DisplayName("성공 - 정상 뮤지컬 상세 이미지 값이 들어오면 저장에 성공한다")
+        void saveSuccess() {
+            // given
+            MusicalDetailImage musicalDetailImage1 = MusicalDetailImage.builder()
+                    .originalFileName("업로드 파일1")
+                    .imageUrl("a")
+                    .musical(musical)
+                    .build();
+            MusicalDetailImage musicalDetailImage2 = MusicalDetailImage.builder()
+                    .originalFileName("업로드 파일2")
+                    .imageUrl("ab")
+                    .musical(musical)
+                    .build();
+            List<MusicalDetailImage> musicalDetailImages = List.of(musicalDetailImage1, musicalDetailImage2);
+            when(musicalDetailImageRepository.save(any(MusicalDetailImage.class))).thenReturn(any(MusicalDetailImage.class));
 
-        // then
-        verify(musicalDetailImageRepository, times(musicalDetailImages.size())).save(any(MusicalDetailImage.class));
+            // when
+            musicalDetailImageService.save(musicalDetailImages);
+
+            // then
+            verify(musicalDetailImageRepository, times(musicalDetailImages.size())).save(any(MusicalDetailImage.class));
+        }
+
     }
 }
