3b3e9aa0c0fbb1a9ec76735952869603021a5235
park-se-jun <tpwns6693@gmail.com>
2023-01-27 10:31:47 +0900

    [ADD] application.yml 관련 파일 추가 - 기본적으로 필요한 공용 application.yml 들 추가. - 민감한 정보는 외부 환경변수를 참조하도록 만듦. - h2 database 이용시 user 가 예액어로 걸려있던 문제를 피하기 위해 h2 버전 업 - UploadImageServiceImpl.java 의 file.images를 application.yml을 만들면서 local.images.path로 변경

--- src/main/java/com/prgrms/be/intermark/common/service/UploadImageServiceImpl.java
+++ src/main/java/com/prgrms/be/intermark/common/service/UploadImageServiceImpl.java
@@ -1,23 +1,21 @@
 package com.prgrms.be.intermark.common.service;
 
+import com.prgrms.be.intermark.common.dto.ImageResponseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-
-import com.prgrms.be.intermark.common.dto.ImageResponseDTO;
-
-import lombok.RequiredArgsConstructor;
-
 @Service
 @RequiredArgsConstructor
 public class UploadImageServiceImpl implements UploadImageService {
 
-	@Value("${file.images}")
+	@Value("${local.images.path}")
 	private String savePath;
 
 	@Override
