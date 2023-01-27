a0e580cdcde83872d72c13b29bd686408a021303
Kong <rhdtn311@naver.com>
2023-01-28 02:56:08 +0900

    [TEST] : 뮤지컬 상세 조회 테스트

--- src/test/java/com/prgrms/be/intermark/domain/musical/service/MusicalFacadeServiceTest.java
+++ src/test/java/com/prgrms/be/intermark/domain/musical/service/MusicalFacadeServiceTest.java
@@ -1,10 +1,20 @@
 package com.prgrms.be.intermark.domain.musical.service;
 
 import com.prgrms.be.intermark.common.dto.page.dto.PageResponseDTO;
+import com.prgrms.be.intermark.domain.actor.dto.ActorResponseDTO;
+import com.prgrms.be.intermark.domain.actor.model.Actor;
+import com.prgrms.be.intermark.domain.actor.model.Gender;
+import com.prgrms.be.intermark.domain.actor.repository.ActorRepository;
+import com.prgrms.be.intermark.domain.casting.model.Casting;
+import com.prgrms.be.intermark.domain.casting.repository.CastingRepository;
+import com.prgrms.be.intermark.domain.musical.dto.MusicalDetailImageResponseDTO;
+import com.prgrms.be.intermark.domain.musical.dto.MusicalDetailResponseDTO;
 import com.prgrms.be.intermark.domain.musical.dto.MusicalSummaryResponseDTO;
 import com.prgrms.be.intermark.domain.musical.model.Genre;
 import com.prgrms.be.intermark.domain.musical.model.Musical;
+import com.prgrms.be.intermark.domain.musical.model.MusicalDetailImage;
 import com.prgrms.be.intermark.domain.musical.model.ViewRating;
+import com.prgrms.be.intermark.domain.musical.repository.MusicalDetailImageRepository;
 import com.prgrms.be.intermark.domain.musical.repository.MusicalRepository;
 import com.prgrms.be.intermark.domain.stadium.model.Stadium;
 import com.prgrms.be.intermark.domain.stadium.repository.StadiumRepository;
@@ -13,6 +23,7 @@ import com.prgrms.be.intermark.domain.user.User;
 import com.prgrms.be.intermark.domain.user.UserRole;
 import com.prgrms.be.intermark.domain.user.repository.UserRepository;
 import com.prgrms.be.intermark.util.TestUtil;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +33,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -43,6 +55,15 @@ class MusicalFacadeServiceTest {
 
     @Autowired
     private StadiumRepository stadiumRepository;
+
+    @Autowired
+    private ActorRepository actorRepository;
+
+    @Autowired
+    private CastingRepository castingRepository;
+
+    @Autowired
+    private MusicalDetailImageRepository musicalDetailImageRepository;
 
     @Test
     @DisplayName("Success - 뮤지컬 리스트 조회 시 뮤지컬 정보 리스트로 반환 - findAllMusicals")
@@ -73,5 +94,61 @@ class MusicalFacadeServiceTest {
                     .usingRecursiveComparison()
                     .isEqualTo(result.getData().get(i));
         }
+    }
+
+    @Test
+    @DisplayName("Success - 뮤지컬 상세 조회 시 뮤지컬 정보를 반환 - findMusicalById")
+    void getMusicalSuccess() {
+        // given
+        User user = TestUtil.createUser(Social.GOOGLE, "socialId", "refreshToken", "nickname", UserRole.ADMIN, false, LocalDate.now(), "email@naver.com");
+        Stadium stadium = TestUtil.createStadium("name", "address", "imageUrl");
+        Musical musical = TestUtil.createMusical("title", "description", LocalDate.now(), LocalDate.now().plusDays(5),
+                "thumbnailUrl", ViewRating.ALL, Genre.COMEDY, 60, user, stadium);
+        MusicalDetailImage musicalDetailImage = TestUtil.createMusicalDetailImage("imageUrl", "fileName");
+        musicalDetailImage.setMusical(musical);
+        Actor actor = TestUtil.createActor("actorName", LocalDate.now(), "profileUrl", Gender.MALE);
+        Casting casting = TestUtil.createCasting(actor, musical);
+        casting.setActor(actor);
+        casting.setMusical(musical);
+
+        userRepository.save(user);
+        stadiumRepository.save(stadium);
+        musicalRepository.save(musical);
+        musicalDetailImageRepository.save(musicalDetailImage);
+        actorRepository.save(actor);
+        castingRepository.save(casting);
+
+        MusicalDetailResponseDTO answer = MusicalDetailResponseDTO.builder()
+                .musicalTitle(musical.getTitle())
+                .startDate(musical.getStartDate())
+                .endDate(musical.getEndDate())
+                .rate(musical.getViewRating())
+                .genre(musical.getGenre())
+                .thumbnailUrl(musical.getThumbnailUrl())
+                .description(musical.getDescription())
+                .runningTime(musical.getRunningTime())
+                .stadiumName(stadium.getName())
+                .actors(ActorResponseDTO.listFromCastings(List.of(casting)))
+                .images(MusicalDetailImageResponseDTO.listFrom(musical.getDetailImages()))
+                .build();
+
+        // when
+        MusicalDetailResponseDTO result = musicalFacadeService.findMusicalById(musical.getId());
+
+        // then
+        assertThat(result)
+                .usingRecursiveComparison()
+                .isEqualTo(answer);
+
+    }
+
+    @Test
+    @DisplayName("Fail - 뮤지컬 상세 조회 시 존재하지 않는 뮤지컬인 경우 조회 실패 - findMusicalById")
+    void getMusicalFail() {
+        // given
+        Long notExistsMusicalId = 0L;
+
+        // when, then
+        Assertions.assertThrows(EntityNotFoundException.class, () -> musicalFacadeService.findMusicalById(notExistsMusicalId));
     }
 }
--- src/test/java/com/prgrms/be/intermark/util/TestUtil.java
+++ src/test/java/com/prgrms/be/intermark/util/TestUtil.java
@@ -1,14 +1,24 @@
 package com.prgrms.be.intermark.util;
 
+import com.prgrms.be.intermark.domain.actor.model.Actor;
+import com.prgrms.be.intermark.domain.actor.model.Gender;
+import com.prgrms.be.intermark.domain.casting.model.Casting;
 import com.prgrms.be.intermark.domain.musical.model.Genre;
 import com.prgrms.be.intermark.domain.musical.model.Musical;
+import com.prgrms.be.intermark.domain.musical.model.MusicalDetailImage;
 import com.prgrms.be.intermark.domain.musical.model.ViewRating;
+import com.prgrms.be.intermark.domain.musical_seat.model.MusicalSeat;
+import com.prgrms.be.intermark.domain.schedule.model.Schedule;
+import com.prgrms.be.intermark.domain.schedule_seat.model.ScheduleSeat;
+import com.prgrms.be.intermark.domain.seat.model.Seat;
+import com.prgrms.be.intermark.domain.seatgrade.model.SeatGrade;
 import com.prgrms.be.intermark.domain.stadium.model.Stadium;
 import com.prgrms.be.intermark.domain.user.Social;
 import com.prgrms.be.intermark.domain.user.User;
 import com.prgrms.be.intermark.domain.user.UserRole;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class TestUtil {
 
@@ -48,5 +58,70 @@ public class TestUtil {
                 .address(address)
                 .imageUrl(imageUrl)
                 .build();
+    }
+
+    public static Actor createActor(String name, LocalDate birth, String profileImageUrl, Gender gender) {
+        return Actor.builder()
+                .name(name)
+                .birth(birth)
+                .profileImageUrl(profileImageUrl)
+                .gender(gender)
+                .build();
+    }
+
+    public static Casting createCasting(Actor actor, Musical musical) {
+        return Casting.builder()
+                .actor(actor)
+                .musical(musical)
+                .build();
+    }
+
+    public static MusicalDetailImage createMusicalDetailImage(String imageUrl, String originalFileName) {
+        return MusicalDetailImage.builder()
+                .imageUrl(imageUrl)
+                .originalFileName(originalFileName)
+                .build();
+    }
+
+    public static MusicalSeat createMusicalSeat(Musical musical, Seat seat, SeatGrade seatGrade) {
+        return MusicalSeat.builder()
+                .musical(musical)
+                .seat(seat)
+                .seatGrade(seatGrade)
+                .build();
+    }
+
+    public static Seat createSeat(String rowNum, int columnNum, Stadium stadium) {
+        return Seat.builder()
+                .rowNum(rowNum)
+                .columnNum(columnNum)
+                .stadium(stadium)
+                .build();
+    }
+
+    public static SeatGrade createSeatGrade(String name, int price, Musical musical) {
+        return SeatGrade.builder()
+                .name(name)
+                .price(price)
+                .musical(musical)
+                .build();
+    }
+
+    public static ScheduleSeat createScheduleSeat(boolean isReserved, Seat seat, SeatGrade seatGrade, Schedule schedule) {
+        return ScheduleSeat.builder()
+                .isReserved(isReserved)
+                .seat(seat)
+                .seatGrade(seatGrade)
+                .schedule(schedule)
+                .build();
+    }
+
+    public static Schedule createSchedule(LocalDateTime startTime, LocalDateTime endTime, Musical musical) {
+        return Schedule.builder()
+                .startTime(startTime)
+                .endTime(endTime)
+                .musical(musical)
+                .build();
+
     }
 }
