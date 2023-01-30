7db11803d3e815ec84c74b0b68a401d326922b79
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2023-01-30 20:29:24 +0900

    [TEST] : 티켓 예매 등록 API 테스트 (#132)
    Co-authored-by: Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
    Co-authored-by: 박세준 <tpwns6693@gmail.com>
    Co-authored-by: So Hyeon <thgus345@naver.com>
    Co-authored-by: 권성준 <tjdwns1865@naver.com>
    Co-authored-by: Seongjun Kwon <82152173+Seongjun-Kwon@users.noreply.github.com>

---
+++ src/test/java/com/prgrms/be/intermark/domain/ticket/controller/TicketControllerTest.java
@@ -0,0 +1,89 @@
+package com.prgrms.be.intermark.domain.ticket.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.prgrms.be.intermark.domain.ticket.dto.TicketCreateRequestDTO;
+import com.prgrms.be.intermark.domain.ticket.service.TicketService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(TicketController.class)
+@ExtendWith(RestDocumentationExtension.class)
+@AutoConfigureRestDocs
+class TicketControllerTest {
+
+    @InjectMocks
+    private TicketController ticketController;
+
+    @MockBean
+    private TicketService ticketService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    @WithMockUser(username = "1", roles = {"ADMIN"}, password = "")
+    @Test
+    @DisplayName("Success - 예매 등록 시 created 상태 코드 반환 및 Location 에 URI 반환 - createTicket")
+    void getAllMusicalsSuccess() throws Exception {
+        // given
+        TicketCreateRequestDTO request = TicketCreateRequestDTO.builder()
+                .userId(1L)
+                .scheduleSeatId(1L)
+                .build();
+
+        Long savedTicketId = 1L;
+
+        when(ticketService.createTicket(any(TicketCreateRequestDTO.class)))
+                .thenReturn(savedTicketId);
+
+        // when
+        ResultActions resultActions = mockMvc.perform(
+                post("/api/v1/tickets")
+                        .contentType("application/json")
+                        .content(mapper.writeValueAsString(request)).with(csrf())
+        );
+
+        // then
+        verify(ticketService).createTicket(any(TicketCreateRequestDTO.class));
+
+        resultActions.andExpect(status().isCreated())
+                .andExpect(header().string("Location", "/api/v1/tickets/" + savedTicketId))
+                .andDo(print())
+                .andDo(document("Save Ticket",
+                                requestFields(
+                                        fieldWithPath("userId").type(JsonFieldType.NUMBER).description("예매하려는 사용자 id"),
+                                        fieldWithPath("scheduleSeatId").type(JsonFieldType.NUMBER).description("예매하려는 스케줄좌석 id")
+                                ),
+                                responseHeaders(
+                                        headerWithName("Location").description("예매된 ticket 의 URI")
+                                )
+                        )
+                );
+    }
+}
