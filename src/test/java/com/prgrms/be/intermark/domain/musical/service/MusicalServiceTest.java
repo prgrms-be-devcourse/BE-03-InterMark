e7d9fabf825f3f5ecc62930aad7f2d51c917ffec
권성준 <tjdwns1865@naver.com>
2023-01-30 14:43:30 +0900

    [TEST] : MusicalService deleteMusical 메서드 테스트 작성

--- src/test/java/com/prgrms/be/intermark/domain/musical/service/MusicalServiceTest.java
+++ src/test/java/com/prgrms/be/intermark/domain/musical/service/MusicalServiceTest.java
@@ -8,12 +8,16 @@ import com.prgrms.be.intermark.domain.util.MusicalProvider;
 import com.prgrms.be.intermark.domain.util.StadiumProvider;
 import com.prgrms.be.intermark.domain.util.UserProvider;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import javax.persistence.EntityNotFoundException;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -25,11 +29,13 @@ class MusicalServiceTest {
     @Mock
     private MusicalRepository musicalRepository;
 
+    @Mock
+    private Musical savedMusical;
+
     private final String thumbnailUrl = "https://intermark.com";
     private final Stadium stadium = StadiumProvider.createStadium();
     private final User user = UserProvider.createUser();
     private final Musical musical = MusicalProvider.createMusical(thumbnailUrl, stadium, user);
-
 
     @Test
     @DisplayName("성공 - 올바른 값이 들어오면 뮤지컬을 저장한다 - save")
@@ -42,5 +48,37 @@ class MusicalServiceTest {
 
         // then
         verify(musicalRepository).save(musical);
+    }
+
+    @Nested
+    @DisplayName("deleteMusical")
+    class DeleteMusical {
+
+        @Test
+        @DisplayName("성공 - 삭제할 뮤지컬이 있으면 삭제에 성공한다.")
+        void deleteMusicalSuccess() {
+            // given
+            when(savedMusical.isDeleted()).thenReturn(false);
+
+            // when
+            musicalService.deleteMusical(savedMusical);
+
+            // then
+            verify(savedMusical).isDeleted();
+            verify(savedMusical).deleteMusical();
+        }
+
+        @Test()
+        @DisplayName("실패 - 이미 삭제된 뮤지컬이면 삭제에 실패한다.")
+        void deleteMusicalFail() {
+            // given
+            when(savedMusical.isDeleted()).thenReturn(true);
+
+            // then
+            assertThatThrownBy(() -> musicalService.deleteMusical(savedMusical))
+                    .isInstanceOf(EntityNotFoundException.class);
+            verify(savedMusical).isDeleted();
+            verify(savedMusical, never()).deleteMusical();
+        }
     }
 }
