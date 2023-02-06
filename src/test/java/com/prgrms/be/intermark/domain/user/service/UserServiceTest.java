1728e35e92ee9573e90f95411d4ea54d72f44198
park-se-jun <tpwns6693@gmail.com>
2023-02-06 21:31:54 +0900

    [TEST] UserService 테스트 추가 - delete 메소드 테스트 추가 - updateRole 메소드 테스트 추가 - changeRefreshToken 테스트 추가 - assignRefreshToken 테스트 추가 - countAllUser 테스트 추가

--- src/test/java/com/prgrms/be/intermark/domain/user/service/UserServiceTest.java
+++ src/test/java/com/prgrms/be/intermark/domain/user/service/UserServiceTest.java
@@ -18,6 +18,7 @@ import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
+import static com.prgrms.be.intermark.auth.constant.JwtConstants.THREE_DAYS_MSEC;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -29,6 +30,148 @@ class UserServiceTest {
     private final UserRepository userRepository = mock(UserRepository.class);
     private final TokenProvider tokenProvider = mock(TokenProvider.class);
     private final UserService userService = new UserService(userRepository, tokenProvider);
+    private final User mockUser = mock(User.class);
+
+    @Nested
+    @DisplayName("userId, role, refreshToken을 입력받아 새로운 role로 갱신된 refreshToken을 반환")
+    class ChangeRefreshToken {
+        @Test
+        @DisplayName("Success - userId,role, refreshToken을 입력받아 새로운 RefreshToken 을 반환.")
+        public void changeRefreshTokenSuccess() {
+            Long userId = 1L;
+            UserRole userRole = UserRole.ROLE_USER;
+            String refreshToken = "";
+            String newRefreshToken = "123";
+            when(userRepository.findByIdAndRefreshToken(userId, refreshToken)).thenReturn(Optional.of(mockUser));
+            when(tokenProvider.getExpiration(any())).thenReturn(THREE_DAYS_MSEC - 10L);
+            when(tokenProvider.createRefreshToken(userId, userRole)).thenReturn(newRefreshToken);
+
+            assertThat(userService.changeRefreshToken(userId, userRole, refreshToken)).isEqualTo(Optional.of(newRefreshToken));
+
+            verify(tokenProvider).getExpiration(any());
+            verify(userRepository).findByIdAndRefreshToken(userId, refreshToken);
+            verify(mockUser).setRefreshToken(newRefreshToken);
+            verify(mockUser).getRefreshToken();
+
+
+        }
+
+        @Test
+        @DisplayName("Fail - userId혹은 refreshToken이 잘못됐을 경우 일 경우 IllegalArgumentException을 반환한다.")
+        public void changeRefreshTokenCannotFindTargetUserFail() {
+            Long userId = 1L;
+            UserRole userRole = UserRole.ROLE_USER;
+            String refreshToken = "";
+
+            when(userRepository.findByIdAndRefreshToken(any(), any())).thenReturn(Optional.empty());
+
+            assertThatThrownBy(() -> userService.changeRefreshToken(userId, userRole, refreshToken)).isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        @DisplayName("Success - 만약 찾은 user의 refreshToken이 3일보다 긴 유효기간을 가진다면, Optional.empty()를 반환")
+        public void changeRefreshTokenExpirationTooLongSuccess() {
+            Long userId = 1L;
+            UserRole userRole = UserRole.ROLE_USER;
+            String refreshToken = "";
+
+            when(userRepository.findByIdAndRefreshToken(userId, refreshToken)).thenReturn(Optional.of(mockUser));
+            when(tokenProvider.getExpiration(any())).thenReturn(THREE_DAYS_MSEC + 10L);
+
+            assertThat(userService.changeRefreshToken(userId, userRole, refreshToken)).isEqualTo(Optional.empty());
+            verify(tokenProvider, never()).createRefreshToken(userId, userRole);
+
+
+        }
+    }
+
+    @Nested
+    @DisplayName("refreshToken을 받아서 해당하는 User에게 refreshToken을 셋팅 해준다.")
+    class AssignRefreshToken {
+        @Test
+        @DisplayName("Success - refreshToken을 받아서 해당하는 user에게 셋팅해준다")
+        public void assignRefreshTokenSucess() {
+            String refreshToken = "";
+            String userIdString = "1";
+            when(tokenProvider.getUserIdFromRefreshToken(any())).thenReturn(userIdString);
+            when(userRepository.findById(1L)).thenReturn(Optional.of(mockUser));
+
+            userService.assignRefreshToken(refreshToken);
+
+            verify(mockUser).setRefreshToken("");
+        }
+
+        @Test
+        @DisplayName("Fail - refreshToken을 받는데 refreshToken이 잘못됐다(일치하는 ID 없음).")
+        public void assignRefreshTokenInvalidRefreshTokenFail() {
+            String refreshToken = "";
+            when(tokenProvider.getUserIdFromRefreshToken(any())).thenReturn("1");
+
+            when(userRepository.findById(any())).thenReturn(Optional.empty());
+            assertThatThrownBy(() -> userService.assignRefreshToken(refreshToken)).isInstanceOf(EntityNotFoundException.class);
+        }
+    }
+
+
+    @Nested
+    class delete {
+        @Test
+        @DisplayName("Success - uerID를 받아서 user를 삭제한다.")
+        public void deleteSuccess() {
+            Long userId = 1L;
+            when(userRepository.findByIdAndIsDeletedFalse(userId)).thenReturn(Optional.of(mockUser));
+
+            userService.delete(userId);
+
+            verify(userRepository).findByIdAndIsDeletedFalse(userId);
+            verify(mockUser).deleteUser();
+        }
+
+        @Test
+        @DisplayName("Fail - userId로 user를 찾을 수 없습니다. EntityNotFoundException 을 던진다")
+        public void deleteWrongUserIdFail() {
+            Long userId = 1L;
+            when(userRepository.findByIdAndIsDeletedFalse(userId)).thenReturn(Optional.empty());
+
+            assertThatThrownBy(() -> userService.delete(userId)).isInstanceOf(EntityNotFoundException.class);
+
+            verify(userRepository, only()).findByIdAndIsDeletedFalse(userId);
+            verify(mockUser, never()).deleteUser();
+
+        }
+    }
+
+    @Nested
+    class updateRole {
+        @Test
+        @DisplayName("Success - userId로 user를 찾아 권한을 업데이트 한다.")
+        public void updateRoleSuccess() {
+
+            Long userId = 1L;
+            UserRole userRole = UserRole.ROLE_ADMIN;
+            when(userRepository.findByIdAndIsDeletedFalse(userId)).thenReturn(Optional.of(mockUser));
+
+            userService.updateRole(userId, userRole);
+
+            verify(userRepository).findByIdAndIsDeletedFalse(userId);
+            verify(mockUser).setRole(userRole);
+
+        }
+
+        @Test
+        @DisplayName("Fail - userId 로 user를 찾지 못해 EntityNotFoundException 을 던진다.")
+        public void updateRoleFail() {
+            Long userId = 1L;
+            UserRole userRole = UserRole.ROLE_ADMIN;
+            when(userRepository.findByIdAndIsDeletedFalse(userId)).thenReturn(Optional.empty());
+
+            assertThatThrownBy(() -> userService.delete(userId)).isInstanceOf(EntityNotFoundException.class);
+
+            verify(userRepository, only()).findByIdAndIsDeletedFalse(userId);
+            verify(mockUser, never()).setRole(any());
+
+        }
+    }
 
     @Nested
     @DisplayName("findByIdAndIsDeletedFalse")
@@ -110,4 +253,12 @@ class UserServiceTest {
         assertThat(response.isNext()).isFalse();
         assertThat(response.isPrev()).isFalse();
     }
+
+    @Test
+    @DisplayName("Success - 삭제되지 않은모든유저의 갯수 조회.")
+    public void countAllUserSuccess() {
+        userService.countAllUser();
+        verify(userRepository, times(1)).countByIsDeletedFalse();
+    }
+
 }
